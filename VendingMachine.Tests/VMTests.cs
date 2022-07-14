2d26a5a6bce5369450d1abfa2083762e9ada274d
keewee75 <marko.kivi@gmail.com>
2022-07-14 09:17:16 +0200

    .

--- VendingMachine.Tests/VMTests.cs
+++ VendingMachine.Tests/VMTests.cs
@@ -16,5 +16,20 @@ namespace VendingMachine.Tests
             //Assert
             Assert.Equal(expected, sut.MoneyPool);
         }
+
+        [Fact]
+
+        public void AddProductToPurchasedListTest()
+        {
+            //Arrange
+            VM sut = new VM();
+            sut.MoneyPool = 100;
+            sut.IncreaseMoneyPool(100);
+
+            //Act
+            int expected = 200;
+            //Assert
+            Assert.Equal(expected, sut.MoneyPool);
+        }
     }
 }
